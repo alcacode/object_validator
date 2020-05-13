e90cff80cf9a3dad828fbe583031ff126c27a86c
alcacode <alca_addr@protonmail.com>
2020-05-14 00:40:56 +0200

    Added clarifying comment

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,11 @@
+/**
+ * Internally used declarations. Some of which might also exist in @types.
+ */
+
 declare type TypedArrayInstance =
-        (Int8Array|Int16Array|Int32Array|BigInt64Array|Uint8Array|
-                Uint8ClampedArray|Uint16Array|Uint32Array|BigUint64Array|
-                Float32Array|Float64Array);
+	(Int8Array | Int16Array | Int32Array | BigInt64Array | Uint8Array |
+	 Uint8ClampedArray | Uint16Array | Uint32Array | BigUint64Array |
+	 Float32Array | Float64Array);
 
 declare interface ArrayLike<T>{
         [Symbol.iterator](): IterableIterator<T>
